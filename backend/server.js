b987406addc8a853d3be88471175da9d4610fba9
Godstime <lewisagho47@gmail.com>
2019-10-16 18:02:53 +0100

    Set-up backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+const http = require('http');
+const app = require('./app');
+
+
+const port = process.env.PORT || 3100;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log('Server restarted successfully');
+});
